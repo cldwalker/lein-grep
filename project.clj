a33f9ff059e47f4a712e12ce0cc122697fe16307
Gabriel Horner <gabriel.horner@gmail.com>
2013-02-01 05:27:11 -0500

    may as well

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "https://en.wikipedia.org/wiki/MIT_License"}
   :dependencies  [[org.clojure/clojure "1.5.0-RC4"]
                   [table "0.3.2"]]
+  :min-lein-version "2.0.0"
   :eval-in-leiningen true)
