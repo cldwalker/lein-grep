08fc82d6be88e0312c144aeee3371dddbe4f7848
Gabriel Horner <gabriel.horner@gmail.com>
2015-05-22 17:35:56 -0400

    Prep for 0.1.1 release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,6 @@
+== 0.1.1
+* Allow lein-grep to run outside of clojure project
+* Bump to latest table
+
 == 0.1.0
 * Initial release!
--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "The MIT License"
             :url "https://en.wikipedia.org/wiki/MIT_License"}
   :dependencies  [[org.clojure/clojure "1.6.0"]
-                  [table "0.4.0"]]
+                  [table "0.5.0"]]
   :min-lein-version "2.0.0"
   :eval-in-leiningen true)
